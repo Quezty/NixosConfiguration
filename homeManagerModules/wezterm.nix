a534678c88dfcc9eb878a795a8e848cc9a44a384
Quezty <storlijoachim@gmail.com>
2024-08-10 18:00:51 +0200

    Added keybind to wezterm

--- homeManagerModules/wezterm.nix
+++ homeManagerModules/wezterm.nix
@@ -21,7 +21,12 @@
           enable_wayland = false,
           hide_tab_bar_if_only_one_tab = true,
           window_background_opacity = 0.5,
-          font = wezterm.font("JetBrainsMono Nerd Font", {weight="Bold", stretch="Normal", style="Normal"})        
+          font = wezterm.font("JetBrainsMono Nerd Font", {weight="Bold", stretch="Normal", style="Normal"}),
+          leader = {key="Space", mods = "CTRL", timeout_milliseconds = 1000},
+          keys = {
+            {key="v", mods="LEADER", action = wezterm.action.SplitVertical ({domain = "CurrentPaneDomain"})},
+            {key="h", mods="LEADER", action = wezterm.action.SplitHorizontal ({domain = "CurrentPaneDomain"})}
+          }
         }
       '';
     };
