7ec2c40e1eeda0570df9fd0f5a775c4f11bef71c
quezty <storlijoachim@gmail.com>
2024-08-21 16:57:58 +0200

    bluetooth
    Added bluetooth support module

--- nixosModules/bluetooth.nix
+++ nixosModules/bluetooth.nix
@@ -16,7 +16,6 @@
         enable = true;
         powerOnBoot = true;
       };
-      pulseaudio.enable = true;
     };
 
     services.blueman.enable = true;
--- nixosModules/default.nix
+++ nixosModules/default.nix
@@ -1,6 +1,7 @@
 { ... }:
 {
   imports = [
+    ./bluetooth.nix
     ./dev.nix
     ./distrobox.nix
     ./tmux.nix
