aee1512fedc7bf24405c9e5ba182fb66a93b1805
Joachim Storli <122435731+Quezty@users.noreply.github.com>
2024-09-10 13:35:59 +0200

    Create home.nix for headless host

---
+++ hosts/headless/home.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ../../homeManagerModules/default.nix
+  ];
+
+
+  home.username = "joachimos";
+  home.homeDirectory = "/home/joachimos";
+  
+  useWezterm.enable = false;
+  useKitty.enable = false;
+  addGit.enable = true;
+  addZsh.enable = true;
+
+
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
