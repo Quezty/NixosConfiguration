fc2fd3f7f3b564c8eeda0309fa2fea3aa0c33990
Joachim Storli <122435731+Quezty@users.noreply.github.com>
2024-09-10 13:31:49 +0200

    Update configuration.nix for headless host

--- hosts/headless/configuration.nix
+++ hosts/headless/configuration.nix
@@ -1,1 +1,73 @@
+{ inputs, config, pkgs, ... }:
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
 
+  # Disabling or enabling my own modules  
+  useHyprland.enable = false;
+  devPackages.enable = true;
+  generalPackages.enable = true; 
+  sshPort22.enable = false;
+  addBluetooth.enable = false;
+  addSound.enable = false;
+
+
+  programs.zsh.enable = true;
+
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Oslo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us, no";
+    variant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.joachimos = {
+    isNormalUser = true;
+    description = "Joachim Olsen Storli";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # List packages installed in system profile.
+  environment.systemPackages = with pkgs; [
+    vim 
+    brightnessctl
+    inputs.my-nixvim.packages.${system}.default
+  ];
+
+  fonts.packages = with pkgs; [
+    nerdfonts
+    font-awesome_5
+  ];
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    wireplumber.enable = true;
+  };
+}
