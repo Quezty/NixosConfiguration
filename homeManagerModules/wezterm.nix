ebcf96235a80bf06e03b0991a875053d5d1dcb30
Quezty <storlijoachim@gmail.com>
2026-06-01 08:10:54 +0200

    changed wezterm to lua config with xdg.configFile

--- homeManagerModules/wezterm.nix
+++ homeManagerModules/wezterm.nix
@@ -15,59 +15,14 @@
   config =
     lib.mkIf config.useWezterm.enable
     {
-      programs.wezterm = {
-        enable = true;
-        extraConfig = ''
-          return {
-            color_scheme = "Noctalia",
-            -- [[window_background_opacity = 0.5,]] --
-            enable_wayland = false,
-            enable_tab_bar = false,
-            font = wezterm.font("iosevka nerd font", {weight="Bold", stretch="Normal", style="Normal"}),
-            leader = {key="Space", mods = "CTRL", timeout_milliseconds = 1000},
-            keys = {
-              {key="u", mods="LEADER", action = wezterm.action.SplitVertical ({domain = "CurrentPaneDomain"})},
-              {key="i", mods="LEADER", action = wezterm.action.SplitHorizontal ({domain = "CurrentPaneDomain"})},
-              {key="c", mods="LEADER", action = wezterm.action.SpawnTab("CurrentPaneDomain")},
-              {key="p", mods="LEADER", action = wezterm.action.MoveTabRelative(-1)},
-              {key="n", mods="LEADER", action = wezterm.action.MoveTabRelative(1)},
-              {key="x", mods="LEADER", action = wezterm.action.CloseCurrentPane({ confirm = true })},
-              {key="h", mods="CTRL", action = wezterm.action.ActivatePaneDirection("Left")},
-              {key="j", mods="CTRL", action = wezterm.action.ActivatePaneDirection("Down")},
-              {key="k", mods="CTRL", action = wezterm.action.ActivatePaneDirection("Up")},
-              {key="l", mods="CTRL", action = wezterm.action.ActivatePaneDirection("Right")}
-            },
+      programs = {
+        wezterm.enable = true;
+        zoxide.enable = true;
+      };
 
-            inactive_pane_hsb = {
-              saturation = 0.74,
-              brightness = 0.4
-            },
-
-            background = {
-              {
-                source = {
-                  File = "/home/joachims/repos/hyprlandconf/backgrounds/samuraiNord.jpg",
-                },
-                hsb = {
-                  hue = 1.0,
-                  saturation = 1.0,
-                  brightness = 0.25,
-                },
-                width = "100%",
-                height = "100%",
-                opacity = 0,
-              },
-              {
-                source = {
-                  Color = "3c3836",
-                },
-                width = "100%",
-                height = "100%",
-                opacity = 0.15,
-              }
-            }
-          }
-        '';
+      xdg.configFile."wezterm" = {
+        source = ./wezterm;
+        recursive = true;
       };
     };
 }
