06b635417edfaa3a24ca09e78ef11606dd5a2c56
Quezty <storlijoachim@gmail.com>
2026-08-04 01:55:26 +0200

    changed to correct noctalia-shell input

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     };
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
+      url = "github:noctalia-dev/noctalia/legacy-v4";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
