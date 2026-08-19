20b206ba32763a19a5a6430f44a95b4f146f0f0e
Quezty <storlijoachim@gmail.com>
2026-08-20 00:03:44 +0200

    changed to noctalia-shell v5

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     };
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia/legacy-v4";
+      url = "github:noctalia-dev/noctalia";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
